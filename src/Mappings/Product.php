50c54b70ea300dafd1a2036b7101ebc017b50d30
Alexandru Dumitru <office@webdal.ro>
2020-04-07 13:40:08 +0300

    Adding product id in SDK

--- src/Mappings/Product.php
+++ src/Mappings/Product.php
@@ -7,6 +7,11 @@ namespace Dsuniq\DsuniqSdk\Mappings;
 class Product extends BaseMapping
 {
     /**
+     * @var int
+     */
+    private $id;
+
+    /**
      * @var string
      */
     private $name;
@@ -65,6 +70,17 @@ class Product extends BaseMapping
      * @var string
      */
     private $dataProviderName;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): self
+    {
+        $this->id = $id;
+        return $this;
+    }
 
     public function getName(): string
     {
